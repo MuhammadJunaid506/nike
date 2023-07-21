437747da129e4c71331d5b18319a543ff06aea62
MuhammadJunaid506 <93211293+MuhammadJunaid506@users.noreply.github.com>
2023-07-22 00:46:36 +0500

    initial commit

---
+++ src/components/Hero.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import { styled } from 'styled-components'
+
+const Hero = () => {
+  return (
+    <MainSection>
+      <div className='section_left'>
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+        <div className='buttons'>
+        <button className='btn'>Shop Now</button>
+        <button className='btn'>Category</button>
+        </div>
+      </div>
+      <div className='section_right'>
+        <img src='./images/hero-image.png' alt='hero image'/>
+      </div>
+    </MainSection>
+  )
+}
+
+export default Hero
+
+const MainSection = styled.main`
+    display:flex;
+    flex-wrap:wrap;
+    padding:0px auto;
+    .section_left{
+        display:flex;
+        flex-direction:column;
+        width:50%;
+        justify-content:center;
+        margin-left:20px;
+    }
+    .section_left h1{
+        font-size:108px;
+        font-weight:900px;
+        line-height: 102px;
+    }
+    .section_left p{
+      font-size:15px;
+      margin-top:10px;
+      margin-bottom:10px;
+      width:80%;
+      line-height:20px;
+      letter-spacing:1px
+    }
+    .buttons{
+      display:flex;
+      gap:14px
+    }
+    .btn{
+        align-items:center;
+        padding: 10px;
+        justify-content:center;
+        background-color: #D01C28;
+        border:none;
+        border-radius:5px;
+        color:white
+    }
+    .section_right img {
+        object-fit:cover;
+
+    }
+`
---
+++ src/components/Nav.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import styled from "styled-components";
+
+const Nav = () => {
+  return (
+    <Navbar>
+        <div className='nav_left'>
+        <img src='./images/brand_logo.png' alt='brand logo'/>
+        </div>
+        <div className='nav_right'>
+            <ul>
+                <li>Menu</li>
+                <li>Location</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+            <button className='btn'>login</button>
+
+      
+    </Navbar>
+  )
+}
+
+export default Nav
+
+const Navbar = styled.div`
+    display:flex;
+    justify-content:space-between;
+    text-align:center;
+    align-items:center;
+    margin:15px;
+    .nav_right {
+       display:flex;
+       width:60%;
+       justify-content:center;
+       padding: 0 auto;
+       align-items:center;
+       text-align:center;
+    }
+    .nav_right ul{
+        list-style:none;
+        display:flex;
+        gap:20px;
+        font-size:16px;
+        font-weight:600px
+    }
+    .nav_right ul li{
+        cursor: pointer;
+
+    }
+    .nav_right ul li:hover{
+        text-decoration:underline;
+        text-decoration-color:var(--red)
+    }
+
+    .btn{
+        align-items:center;
+        padding: 10px;
+        justify-content:center;
+        height:50%;
+        background-color: var(--red);
+        border:none;
+        border-radius:5px;
+        color:white
+    }
+`
